661f8f382008525e9bd21c7b1ba3182ffc0caa6e
Iago Teles <telesiago@gmail.com>
2025-03-10 22:57:22 -0300

    loading CSS

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,19 @@ module.exports = {
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "index.html")
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets", "scissors.svg"),
         }),
     ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 
 
 }
